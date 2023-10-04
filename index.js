aaa0a8b02df7fa414e4739e6981090d5191ddc36
UsmanSaeed787 <usmanalisaeed787@gmail.com>
2023-10-04 23:59:20 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+let rocker; // Type: any
+rocker = "Usman Saeed"; // Type: string
+console.log(rocker);
+rocker = "19.58"; // Type: number
+console.log(rocker);
+let rapper = "Queen Latifah";
+console.log(rapper.length); // ok
+let cher = {
+    firstName: "Cherilyn",
+    lastName: "Sarkisian",
+};
+console.log(cher);
---
+++ index.ts
@@ -0,0 +1,13 @@
+let rocker:any; // Type: any
+rocker = "Usman Saeed"; // Type: string
+console.log(rocker)
+rocker = "19.58"; // Type: number
+console.log(rocker)
+let rapper = "Queen Latifah";
+console.log( rapper.length); // ok
+
+let cher = {
+    firstName: "Cherilyn",
+    lastName: "Sarkisian",
+    };
+    console.log(cher)
